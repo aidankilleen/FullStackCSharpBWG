88a56ba9018e0a0aaf706938a694e70dc7735e99
Aidan Killeen <aidan.killeen@gmail.com>
2023-06-14 16:13:03 +0100

    added db hello world

---
+++ SqlServerHelloWorld/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Data.SqlClient;
+
+Console.WriteLine("SQL Server Hello World");
+
+var connectionString = @"Server=tcp:professionaltraining.database.windows.net,1433;Initial Catalog=trainingdb;Persist Security Info=False;User ID=ptdbuser;Password=xxxxxx;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+
+SqlConnection conn = new SqlConnection(connectionString);
+
+conn.Open();
+
+var sql = "SELECT * FROM members";
+
+SqlCommand cmd = new SqlCommand(sql, conn);
+SqlDataReader rdr = cmd.ExecuteReader();
+
+while (rdr.Read())
+{
+    Console.WriteLine(rdr.GetString(1));
+}
+
+rdr.Close();
+conn.Close();
+
+
+
