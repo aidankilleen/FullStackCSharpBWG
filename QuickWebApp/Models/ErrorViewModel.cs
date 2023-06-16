9c16c180bbc35c476d9243065ce54bc1b0a244fc
Aidan Killeen <aidan.killeen@gmail.com>
2023-06-16 16:03:59 +0100

    final commit

---
+++ QuickWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace QuickWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
